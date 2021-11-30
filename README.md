8e11b71b920e72df458437102f4c9d2e8093fea5
Matt Clarke <matt.clarke@esss.se>
2021-11-30 07:26:59 +0100

    set up project

---
+++ README.md
@@ -0,0 +1,6 @@
+# advent_of_code_21
+https://adventofcode.com/2021
+
+## Day 1
+- Part 1: looping and tracking
+- Part 2: put empty border around keypad to keep boundary checks simple
---
+++ day_1/day_1.py
@@ -0,0 +1,13 @@
+with open("input.txt") as f:
+    PUZZLE_INPUT = f.read()
+
+# PUZZLE_INPUT = """
+# R2, L3
+# """
+
+puzzle_input = [x.strip() for x in PUZZLE_INPUT.strip().split(",")]
+print(puzzle_input)
+
+
+# Part 1 = 287
+# print(f"answer = {}")
