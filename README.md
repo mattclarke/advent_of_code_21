5acd63cee4e1631b1ace7db2f8ce8fcc40ad81a9
Matt Clarke <matt.clarke@esss.se>
2021-12-01 18:36:37 +0100

    Day 1 done

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 https://adventofcode.com/2021
 
 ## Day 1
-- Part 1: looping and tracking
-- Part 2: put empty border around keypad to keep boundary checks simple
+- Part 1: looping, comparing and counting.
+- Part 2: loop once to collect the group of threes and then do the counting in a separate loop.
+Can we do it in one loop?
--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,12 +2,52 @@ with open("input.txt") as f:
     PUZZLE_INPUT = f.read()
 
 # PUZZLE_INPUT = """
-# R2, L3
+# 199
+# 200
+# 208
+# 210
+# 200
+# 207
+# 240
+# 269
+# 260
+# 263
 # """
 
-puzzle_input = [x.strip() for x in PUZZLE_INPUT.strip().split(",")]
+puzzle_input = [int(x.strip()) for x in PUZZLE_INPUT.strip().split("\n")]
 print(puzzle_input)
 
+previous = None
+count = 0
 
-# Part 1 = 287
-# print(f"answer = {}")
+for depth in puzzle_input:
+    if previous is None:
+        previous = depth
+        continue
+    if depth > previous:
+        count += 1
+    previous = depth
+
+# Part 1 = 1688
+print(f"answer = {count}")
+
+window = puzzle_input[0:3]
+scores = [sum(window)]
+previous = None
+count = 0
+
+for depth in puzzle_input[3:]:
+    window.pop(0)
+    window.append(depth)
+    scores.append(sum(window))
+
+for score in scores:
+    if previous is None:
+        previous = score
+        continue
+    if score > previous:
+        count += 1
+    previous = score
+
+# Part 2 = 1728
+print(f"answer = {count}")
