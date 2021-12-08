383645eed25a7d50875a5ad2bb760af1d7ef089f
Matt Clarke <matt.clarke@esss.se>
2021-12-08 08:50:38 +0100

    Day 8 done.

--- README.md
+++ README.md
@@ -30,3 +30,11 @@ born on a day rather than each individual fish.
 - Part 1: loop over all the possible positions and find the best one.
 - Part 2: need to find the sigma of the difference, first version used recursion and memoisation to find sigma. While
 looking up the name "sigma" found the formula for it: (x*x + x)/2 which is a bit simpler!
+
+## Day 8
+- Part 1: count the number of inputs with a segment lengths corresponding to the digits 1, 4, 7, 8 as they are unique.
+- Part 2: Started going down the wrong path of trying to work out what a to g were individually then realised that was
+not required. Instead we only need to know the "ab" is 1, we don't need to know what "a" is. It is trivial to work
+out 1, 4, 7, 8 because they each have a unique number of segments. The others can be determined by their segment count
+and the number of intersection they have with 1 and 4. E.g. "6" has 6 segments but only has one intersection with "1"
+while "0" and "9" have two intersections with "1" but "9" has more intersections with "4" than "0" does.
---
+++ day_8/day_8.py
@@ -0,0 +1,139 @@
+from functools import lru_cache
+
+with open("input.txt") as f:
+    PUZZLE_INPUT = f.read()
+
+# PUZZLE_INPUT = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+
+# PUZZLE_INPUT = """
+# be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+# edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+# fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+# fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+# aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+# fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+# dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+# bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+# egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+# gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+# """
+
+puzzle_input = [x for x in PUZZLE_INPUT.strip().split("\n")]
+print(puzzle_input)
+
+segments = {
+    0: {"a", "b", "c", "e", "f", "g"},
+    1: {"c", "f"},
+    2: {"a", "c", "d", "e", "g"},
+    3: {"a", "c", "d", "f", "g"},
+    4: {"b", "c", "d", "f"},
+    5: {"a", "b", "d", "f", "g"},
+    6: {"a", "b", "d", "e", "f", "g"},
+    7: {"a", "c", "f"},
+    8: {"a", "b", "c", "d", "e", "f", "g"},
+    9: {"a", "b", "c", "d", "f", "g"},
+}
+
+lines = []
+
+for l in puzzle_input:
+    front, back = l.split(" | ")
+    lines.append((front.split(" "), back.split(" ")))
+print(lines)
+
+
+count = 0
+for l in lines:
+    _, back = l
+    for x in back:
+        if len(x) in [
+            len(segments[1]),
+            len(segments[4]),
+            len(segments[7]),
+            len(segments[8]),
+        ]:
+            count += 1
+
+
+# Part 1 = 342
+print(f"answer = {count}")
+
+
+def solve(line):
+    mapping = {}
+    reverse_mapping = {}
+
+    # Easy ones first
+    front, back = line
+    unsolved = []
+
+    for f in front:
+        f = sorted(f)
+        if len(f) == len(segments[1]):
+            mapping[1] = set(f)
+            reverse_mapping["".join(f)] = 1
+        elif len(f) == len(segments[4]):
+            mapping[4] = set(f)
+            reverse_mapping["".join(f)] = 4
+        elif len(f) == len(segments[7]):
+            mapping[7] = set(f)
+            reverse_mapping["".join(f)] = 7
+        elif len(f) == len(segments[8]):
+            mapping[8] = set(f)
+            reverse_mapping["".join(f)] = 8
+        else:
+            unsolved.append(set(f))
+
+    # Hard ones
+    for f in unsolved:
+        s = "".join(sorted(f))
+        if len(f) == 6:
+            # 0, 6 or 9
+            inter_1 = mapping[1].intersection(f)
+            inter_4 = mapping[4].intersection(f)
+            if len(inter_1) == 1:
+                # 6
+                mapping[6] = f
+                reverse_mapping[s] = 6
+            elif len(inter_4) == 4:
+                # 9
+                mapping[9] = f
+                reverse_mapping[s] = 9
+            else:
+                # 0
+                mapping[0] = f
+                reverse_mapping[s] = 0
+        elif len(f) == 5:
+            # 2, 3 or 5
+            inter_1 = mapping[1].intersection(f)
+            inter_4 = mapping[4].intersection(f)
+            if len(inter_1) == 2:
+                # 3
+                mapping[3] = f
+                reverse_mapping[s] = 3
+            elif len(inter_4) == 3:
+                # 5
+                mapping[5] = f
+                reverse_mapping[s] = 5
+            else:
+                # 2
+                mapping[2] = f
+                reverse_mapping[s] = 2
+
+    sorted_back = []
+    for b in back:
+        b = sorted(b)
+        sorted_back.append("".join(b))
+
+    answer = ""
+    for b in sorted_back:
+        answer += str(reverse_mapping[b])
+    return int(answer)
+
+
+result = 0
+for line in lines:
+    result += solve(line)
+
+# Part 2 = 1068933
+print(f"answer = {result}")
