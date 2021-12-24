15976659c446254d065ca7b2ecf91489b6e2692b
Matt Clarke <matt.clarke@esss.se>
2021-12-24 11:05:18 +0100

    day 23 done

--- day_23/day_23.py
+++ day_23/day_23.py
@@ -1,8 +1,15 @@
 import copy
+from collections import defaultdict
 
 
 # EXAMPLE
 puzzle_input = [
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    ["#", "#", "B", "#", "C", "#", "B", "#", "D", "#", "#"],
+    ["#", "#", "A", "#", "D", "#", "C", "#", "A", "#", "#"],
+]
+
+puzzle_input_2 = [
     [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
     ["#", "#", "B", "#", "C", "#", "B", "#", "D", "#", "#"],
     ["#", "#", "D", "#", "C", "#", "B", "#", "A", "#", "#"],
@@ -10,26 +17,29 @@ puzzle_input = [
     ["#", "#", "A", "#", "D", "#", "C", "#", "A", "#", "#"],
 ]
 
-puzzle_input = [
-    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-    ["#", "#", "B", "#", "C", "#", "B", "#", "D", "#", "#"],
-    ["#", "#", "A", "#", "D", "#", "C", "#", "A", "#", "#"],
-]
 
 # REAL
 # puzzle_input = [
 #     [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 #     ["#", "#", "D", "#", "D", "#", "C", "#", "C", "#", "#"],
-#     ["#", "#", "D", "#", "C", "#", "B", "#", "A", "#", "#"],
-#     ["#", "#", "D", "#", "B", "#", "A", "#", "C", "#", "#"],
 #     ["#", "#", "B", "#", "A", "#", "B", "#", "A", "#", "#"],
 # ]
 
+
+puzzle_input_2 = [
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    ["#", "#", "D", "#", "D", "#", "C", "#", "C", "#", "#"],
+    ["#", "#", "D", "#", "C", "#", "B", "#", "A", "#", "#"],
+    ["#", "#", "D", "#", "B", "#", "A", "#", "C", "#", "#"],
+    ["#", "#", "B", "#", "A", "#", "B", "#", "A", "#", "#"],
+]
+
+
 HOMES = {
-    "A": ((1, 2), (2, 2), (3,2), (4,2)),
-    "B": ((1, 4), (2, 4), (3,4), (4,4)),
-    "C": ((1, 6), (2, 6), (3,6), (4,6)),
-    "D": ((1, 8), (2, 8), (3,8), (4,8)),
+    "A": ((1, 2), (2, 2), (3, 2), (4, 2)),
+    "B": ((1, 4), (2, 4), (3, 4), (4, 4)),
+    "C": ((1, 6), (2, 6), (3, 6), (4, 6)),
+    "D": ((1, 8), (2, 8), (3, 8), (4, 8)),
 }
 
 COSTS = {
@@ -46,9 +56,6 @@ def pprint(board):
     for y in board:
         print("".join(y))
     print()
-
-
-pprint(puzzle_input)
 
 
 def can_reach(start, end, board):
@@ -73,13 +80,13 @@ def can_reach(start, end, board):
 
 
 def is_home(pos, board):
-    ch = board[pos[0]][ pos[1]]
+    ch = board[pos[0]][pos[1]]
     r, c = pos
-    if (r,c) not in HOMES[ch]:
+    if (r, c) not in HOMES[ch]:
         return False
-    r2 = board[r+1][c] if r+1 < len(board) else ch
-    r3 = board[r+2][c] if r+2 < len(board) else ch
-    r4 = board[r+2][c] if r+3 < len(board) else ch
+    r2 = board[r + 1][c] if r + 1 < len(board) else ch
+    r3 = board[r + 2][c] if r + 2 < len(board) else ch
+    r4 = board[r + 2][c] if r + 3 < len(board) else ch
 
     if r2 == ch and r3 == ch and r4 == ch:
         return True
@@ -88,7 +95,7 @@ def is_home(pos, board):
 
 
 def can_move(board):
-    result = []
+    result = defaultdict(lambda: [])
     # Can move home?
     for i, ch in enumerate(board[0]):
         if ch in ["A", "B", "C", "D"]:
@@ -99,107 +106,90 @@ def can_move(board):
             home4 = board[homes[3][0]][homes[3][1]] if len(board) > 3 else ch
             if home4 == ".":
                 if can_reach((0, i), homes[3], board):
-                    result.append((0, i))
+                    result[(0, i)] = [homes[3]]
             elif home3 == "." and home4 == ch:
                 if can_reach((0, i), homes[2], board):
-                    result.append((0, i))
+                    result[(0, i)] = [homes[2]]
             elif home2 == "." and home3 == ch and home4 == ch:
-                # Might be able to go
                 if can_reach((0, i), homes[1], board):
-                    result.append((0, i))
+                    result[(0, i)] = [homes[1]]
             elif home1 == "." and home2 == ch and home3 == ch and home4 == ch:
-                # Might be able to go
                 if can_reach((0, i), homes[0], board):
-                    result.append((0, i))
+                    result[(0, i)] = [homes[0]]
     # Can move out from home row?
     for row in [1, 2, 3, 4]:
         if row >= len(board):
             continue
         for i, ch in enumerate(board[row]):
             if ch in ["A", "B", "C", "D"]:
-                if board[row-1][i] in ["A", "B", "C", "D"]:
+                if board[row - 1][i] in ["A", "B", "C", "D"]:
                     # Blocked
                     continue
                 if is_home((row, i), board):
                     # Home and safe
                     continue
-                left_options = []
+                # Can go straight home
+                homes = HOMES[ch]
+                home1 = board[homes[0][0]][homes[0][1]]
+                home2 = board[homes[1][0]][homes[1][1]]
+                home3 = board[homes[2][0]][homes[2][1]] if len(board) > 3 else ch
+                home4 = board[homes[3][0]][homes[3][1]] if len(board) > 3 else ch
+                if home4 == ".":
+                    if can_reach((row, i), homes[3], board):
+                        result[(row, i)] = [homes[3]]
+                        continue
+                elif home3 == "." and home4 == ch:
+                    if can_reach((row, i), homes[2], board):
+                        result[(row, i)] = [homes[2]]
+                        continue
+                elif home2 == "." and home3 == ch and home4 == ch:
+                    if can_reach((row, i), homes[1], board):
+                        result[(row, i)] = [homes[1]]
+                        continue
+                elif home1 == "." and home2 == ch and home3 == ch and home4 == ch:
+                    if can_reach((row, i), homes[0], board):
+                        result[(row, i)] = [homes[0]]
+                        continue
+
                 for x in range(0, i):
                     if board[0][x] in ["A", "B", "C", "D"]:
-                        left_options.clear()
+                        result[(row, i)].clear()
                     elif (0, x) not in FORBIDDEN:
-                        left_options.append(x)
-                right_options = []
-                blocked = False
+                        result[(row, i)].append((0, x))
+
                 for x in range(i, len(board[0])):
                     if board[0][x] in ["A", "B", "C", "D"]:
-                        blocked = True
-                    elif (0, x) not in FORBIDDEN and not blocked:
-                        right_options.append(x)
-                if left_options or right_options:
-                    result.append((row, i))
+                        break
+                    elif (0, x) not in FORBIDDEN:
+                        result[(row, i)].append((0, x))
     return result
-
-
-def get_options(pos, board):
-    r, c = pos
-    ch = board[r][c]
-    assert ch in ["A", "B", "C", "D"]
-    if r == 0:
-        # Can get home
-        homes = HOMES[ch]
-        home1 = board[homes[0][0]][homes[0][1]]
-        home2 = board[homes[1][0]][homes[1][1]]
-        if home2 == ".":
-            # Might be able to go
-            if can_reach((r, c), homes[1], board):
-                return [homes[1]]
-        elif home1 == "." and home2 == ch:
-            # Might be able to go
-            if can_reach((r, c), homes[0], board):
-                return [homes[0]]
-    else:
-        # Can get home
-        homes = HOMES[ch]
-        home1 = board[homes[0][0]][homes[0][1]]
-        home2 = board[homes[1][0]][homes[1][1]]
-        if home2 == ".":
-            # Might be able to go
-            if can_reach((r, c), homes[1], board):
-                return [homes[1]]
-        elif home1 == "." and home2 == ch:
-            # Might be able to go
-            if can_reach((r, c), homes[0], board):
-                return [homes[0]]
-
-        result = []
-        for x in range(0, c):
-            if board[0][x] in ["A", "B", "C", "D"]:
-                result.clear()
-            elif (0, x) not in FORBIDDEN:
-                result.append((0, x))
-
-        for x in range(c, len(board[0])):
-            if board[0][x] in ["A", "B", "C", "D"]:
-                break
-            elif (0, x) not in FORBIDDEN:
-                result.append((0, x))
-        return result
 
 
 def is_done(board):
     for c in ["A", "B", "C", "D"]:
         home1, home2, home3, home4 = HOMES[c]
         if len(board) == 3:
-            if board[home1[0]][home1[1]] != c or board[home1[0]][home1[1]] != c:
+            if board[home1[0]][home1[1]] != c or board[home2[0]][home2[1]] != c:
                 return False
+        else:
+            if (
+                board[home1[0]][home1[1]] != c
+                or board[home2[0]][home2[1]] != c
+                or board[home3[0]][home3[1]] != c
+                or board[home4[0]][home4[1]] != c
+            ):
+                return False
+
     return True
 
 
-best = 10000000000000
+best = 1000000000000000000
 
 
 def solve(board):
+    global best
+    best = 10000000000000000
+
     def _recurse(board, score):
         global best
         if score >= best:
@@ -213,28 +203,26 @@ def solve(board):
             return
 
         moveable = can_move(board)
-        for mv in moveable:
-            options = get_options(mv, board)
-            # print(mv, " = ", options)
+        for pos, options in moveable.items():
             for o in options:
-                ch = board[mv[0]][mv[1]]
+                ch = board[pos[0]][pos[1]]
                 new_board = copy.deepcopy(board)
-                new_board[o[0]][o[1]], new_board[mv[0]][mv[1]] = (
-                    new_board[mv[0]][mv[1]],
+                new_board[o[0]][o[1]], new_board[pos[0]][pos[1]] = (
+                    new_board[pos[0]][pos[1]],
                     new_board[o[0]][o[1]],
                 )
-                steps = abs(mv[1] - o[1])
-                steps += mv[0]
+                steps = abs(pos[1] - o[1])
+                steps += pos[0]
                 steps += o[0]
                 _recurse(new_board, score + steps * COSTS[ch])
 
     _recurse(board, 0)
-
-solve(copy.deepcopy(puzzle_input))
+    return best
 
 
 # Part 1 = 16059
-# print(f"answer = {total_on}")
+# print(f"answer = {solve(copy.deepcopy(puzzle_input))}")
 
-# Part 2 =
-# print(f"answer = {count}")
+
+# Part 2 = 43117
+print(f"answer = {solve(copy.deepcopy(puzzle_input_2))}")
